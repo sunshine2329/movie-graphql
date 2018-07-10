f3c3961002cd5977cdbbc6cb7f282404e0de3fc5
sunshine2329 <seniordeveloper00001@gmail.com>
2018-07-10 22:35:29 +0100

    Wrapping a REST API with GraphQL

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,8 @@
-import { getMovies, getById } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, {name, score}) => addMovie(name, score)
+    movies: () => getMovies()
   }
 };
 
