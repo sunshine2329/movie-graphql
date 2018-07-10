176007b7ad68e1e59e2d30fcd1921749479e7ac7
sunshine2329 <seniordeveloper00001@gmail.com>
2018-07-10 22:01:30 +0100

    Defining Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "1",
-    name: "Raymond",
-    age: 32,
-    gender: "male"
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: "2",
-    name: "Raymond",
-    age: 32,
-    gender: "male"
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: "3",
-    name: "Raymond",
-    age: 32,
-    gender: "male"
+    id: 2,
+    name: "The Godfather I",
+    score: 99
   },
   {
-    id: "4",
-    name: "Raymond",
-    age: 32,
-    gender: "male"
-  },
-  {
-    id: "5",
-    name: "Raymond",
-    age: 32,
-    gender: "male"
+    id: 3,
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
-}
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
